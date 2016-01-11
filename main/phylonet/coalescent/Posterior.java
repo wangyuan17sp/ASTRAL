387c456a03bed6e57d23de726614799a955b5a69
Siavash Mirarab <smirarab@gmail.com>
2016-01-11 14:48:17 -0800

    update posterior boundary cases

--- main/phylonet/coalescent/Posterior.java
+++ main/phylonet/coalescent/Posterior.java
@@ -1,12 +1,14 @@
 package phylonet.coalescent;
 import cern.jet.stat.*;
-import cern.jet.math.*;
+
 public class Posterior extends cern.jet.math.Constants{
 	private double m1;
 	private double m2;
 	private double m3;
 	private double n;
 	private double posterior;
+	final private static String MESSAGE = "This shouldn't haved happened. Please report error with the following numbers: ";
+	
 	public Posterior(double ft1, double ft2, double ft3, double nt){
 	 	m1 = ft1*nt/(ft1+ft2+ft3);
 		m2 = ft2*nt/(ft1+ft2+ft3);
@@ -33,17 +35,40 @@ public class Posterior extends cern.jet.math.Constants{
 		}
 		return g;
 	}
+	public double r(double mi){
+		return Math.exp(Math.log(2.)*(mi-m1)+betaRatio(mi+1,n-mi+1,m1+1,n-m1+1));
+	}
+	
+	public double m(double mi, double r) {
+		double x = G(mi,n) * r;
+		if (Double.isNaN(x) || Double.isInfinite(x)) {
+			if (mi<n/3) {
+				return 0;
+			} else if (mi==n/3) {
+				if (m1 > mi) {
+					return 0;
+				} else {
+					throw new RuntimeException(MESSAGE + "\n" + m1 +" "+ m2 +" "+ m3 +" ");
+				}
+			} else {
+				return Double.POSITIVE_INFINITY;
+			}
+		}
+		return x;
+	}
 	public double post(){
-		double g2 = Math.log(2.)*(m2-m1)+betaRatio(m2+1,n-m2+1,m1+1,n-m1+1);
-		double g3 = Math.log(2.)*(m3-m1)+betaRatio(m3+1,n-m3+1,m1+1,n-m1+1);
-		g2 = Math.exp(g2)*G(m2,n);
-		g3 = Math.exp(g3)*G(m3,n);
+		double g2 = m(m2,r(m2));
+		double g3 = m(m3,r(m3));
 
 		double g = G(m1,n)/(G(m1,n)+g2+g3);
-		if (Double.isNaN(g) || Double.isInfinite(g)){
-			if (m1/n>1./3.) g=1.;
-			else g=0;
+		
+		if (Double.isNaN(g) || Double.isInfinite(g)) {
+			throw new RuntimeException(MESSAGE + "\n" + m1 +" "+ m2 +" "+ m3 +" ");
 		}
+		/*	if (Double.isNaN(g) || Double.isInfinite(g)){
+			if (m1>m2 & m1>m3) g=1.;
+			else g=0;
+		}*/
 		//if (g == 0){
 		//	double v1 = G(m1,n)/(G(m1,n)+G(m2,n)*Functions.pow.apply(2.,m2-m1)*Gamma.beta(m2+1,n-m2+1)/Gamma.beta(m1+1,n-m1+1)+G(m3,n)*Functions.pow.apply(2.,m3-m1)*Gamma.beta(m3+1,n-m3+1)/Gamma.beta(m1+1,n-m1+1));
 		//	if (Double.isNaN(v1)){
@@ -56,11 +81,25 @@ public class Posterior extends cern.jet.math.Constants{
 	}    
 
 	 public static void main(String[] args) {
-		double m1 = Double.parseDouble(args[0]);
+	/*	double m1 = Double.parseDouble(args[0]);
 		double m2 = Double.parseDouble(args[1]);
 		double m3 = Double.parseDouble(args[2]);
 		double n  = Double.parseDouble(args[3]);
 		Posterior a = new Posterior(m1,m2,m3,n);
-		System.out.println(a.toString());
-        }
+		System.out.println(a.toString());*/
+		 double m1 = 100000-10000;
+		 double m2 = 100000+5000;
+		 double n =  300000;
+		 double m3 = n-m1-m2;
+		 Posterior p = new Posterior(10, 10, 10, n);
+		 p.m1 = m1;p.m2 = m2;p.m3 = m3;
+		 System.out.println(p.G(m1,n));
+		 System.out.println(p.G(m2,n));
+		 System.out.println(p.G(m3,n));
+		 System.out.println("r2: " + p.r(m2));
+		 System.out.println("r3: " + p.r(m3));
+		 System.out.println(p.m1 +" "+ p.m2 +" "+ p.m3);
+		 System.out.println(p.post());
+
+	 }
 }
